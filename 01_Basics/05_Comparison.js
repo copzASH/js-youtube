3ded0093d1bf7d6c4eb6de36027af6f4859a64e4
Himanshu Rathi <himanshurathi588@gmail.com>
2024-03-22 23:43:38 +0000

    Basic Comaprisons

---
+++ 01_Basics/05_Comparison.js
@@ -0,0 +1,24 @@
+// Basic Comparisons
+
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 >= 2);
+// console.log(1 <= 2);
+// console.log(1 == 2);
+
+console.log("2" >= 2);
+console.log("02" >= 2);
+
+// tricky comparisons
+console.log(null > 0)       //false
+console.log(null == 0);     //false
+console.log(null >= 0);     //true
+
+// same comparison in case of undefined results in false in all 3 cases
+
+
+// diff b/w == and ===
+console.log("2" == 2);      //true
+console.log("2" === 2);     //false
+
+// == only checks the value while === checks the value as well as the data type
